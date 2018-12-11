f081f8f83b23202995719cafe072e055a24e44fe
Ricky Do <ricky.do@dps.texas.gov>
2018-12-11 09:38:19 -0600

    chats are submitted, automatic scroll down, and clear message input field after submit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,13 @@ import { combineReducers } from 'redux';
 import contacts from './contacts'; 
 import user from './user';
 import activeUserId from './activeUserId';
+import messages from './messages';
+import typing from './typing';
 
 export default combineReducers( {
     contacts,
     user, 
-    activeUserId
+    activeUserId,
+    messages,
+    typing,
 });
